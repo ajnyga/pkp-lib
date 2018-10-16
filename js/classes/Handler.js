65785285a1b6cdbb7070c9093558a40d06cad67f
ajnyga <ajnyga@gmail.com>
2018-10-16 16:58:34 +0300

    pkp/pkp-lib#2546 Add oneline tinymce editor for title fields

--- js/classes/Handler.js
+++ js/classes/Handler.js
@@ -696,8 +696,17 @@
 						settings = tinyMCE.EditorManager.settings;
 
 				// Set the extended toolbar, if requested
+				settings.inline = false;
 				if ($(this).hasClass('extendedRichContent')) {
 					settings.toolbar = settings.richToolbar;
+				} else if ($(this).hasClass('onelineRichContent')){
+					settings.toolbar = settings.onelineToolbar;
+					settings.forced_root_block = false;
+					settings.menubar = false;
+					settings.statusbar = false;
+					settings.valid_elements = "em/i,sup,sub";
+					settings.branding = false;
+					settings.inline = true;
 				} else {
 					settings.toolbar = settings.defaultToolbar;
 				}
